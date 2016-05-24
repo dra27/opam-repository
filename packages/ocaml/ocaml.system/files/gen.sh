59cfff659cbd1a83b0b945d248d018e4dbfcb3a5
David Allsopp <david.allsopp@metastack.com>
2016-05-24 10:27:43 +0100

    Fix ocaml.system package for Windows

--- packages/ocaml/ocaml.system/files/gen.sh
+++ packages/ocaml/ocaml.system/files/gen.sh
@@ -1,12 +1,32 @@
 #!/bin/bash -ue
 
-if ! OCAMLC=$(which ocamlc); then
+case "$(uname -s)" in
+  CYGWIN*)
+    WINDOWS=true
+    SEP=\\
+    EXE=.exe
+    ;;
+  *)
+    WINDOWS=false
+    SEP=/
+    EXE=
+    ;;
+esac
+
+if ! OCAMLC_SHELL=$(which ocamlc$EXE); then
     echo "No OCaml compiler was found on the system" >&2
     exit 2
 fi
 
-LIBDIR=$("$OCAMLC" -where)
-STUBLIBS=$(cat "$LIBDIR/ld.conf" | tr '\n' ':')
+LIBDIR=$("$OCAMLC_SHELL" -where | tr -d '\r')
+if $WINDOWS ; then
+  LIBDIR_SHELL=$(cygpath "$LIBDIR")
+  OCAMLC=$(cygpath -w "$OCAMLC_SHELL")
+else
+  LIBDIR_SHELL=$LIBDIR
+  OCAMLC=$OCAMLC_SHELL
+fi
+STUBLIBS=$(cat $LIBDIR_SHELL/ld.conf | tr -d '\r' | tr '\n' ':')
 
 echo "Using ocaml compiler found at $OCAMLC with base lib at $LIBDIR"
 
@@ -16,14 +36,14 @@ bool() {
 
 cat >ocaml.config <<EOF
 opam-version: "1.3.0~dev4"
-file-depends: ["$OCAMLC" "$(md5sum "$OCAMLC" | cut -d' ' -f1)"]
+file-depends: ["$(echo "$OCAMLC"| sed -e "s/\\\\/\\\\\\\\/g")" "$(md5sum "$OCAMLC_SHELL" | cut -d' ' -f1)"]
 variables {
-    ocaml-version: "$("$OCAMLC" -version)"
+    ocaml-version: "$("$OCAMLC_SHELL" -version | tr -d '\r')"
     compiler: "system"
     preinstalled: true
-    ocaml-native: $(bool [ -x "$(dirname "$OCAMLC")"/ocamlopt ])
-    ocaml-native-tools: $(bool [ -x "$OCAMLC".opt ])
-    ocaml-native-dynlink: $(bool [ -e "$LIBDIR"/dynlink.cmxa ])
-    ocaml-stubsdir: "$STUBLIBS"
+    ocaml-native: $(bool [ -x "$(dirname "$OCAMLC_SHELL")"/ocamlopt ])
+    ocaml-native-tools: $(bool [ -x "$(dirname "$OCAMLC_SHELL")"/ocamlc.opt$EXE ])
+    ocaml-native-dynlink: $(bool [ -e "$LIBDIR_SHELL"/dynlink.cmxa ])
+    ocaml-stubsdir: "$(echo "$STUBLIBS"| sed -e "s/\\\\/\\\\\\\\/g")"
 }
 EOF
