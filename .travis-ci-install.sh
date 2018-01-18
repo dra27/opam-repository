636b56615b4018465d546af9e0e8a5ba6ddd0215
David Allsopp <david.allsopp@metastack.com>
2018-01-18 11:06:19 +0000

    Use dra27/ocaml-ci-scripts temporarily
    Until PR#??? is merged.

--- .travis-ci-install.sh
+++ .travis-ci-install.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-ocaml.sh
+wget https://raw.githubusercontent.com/dra27/ocaml-ci-scripts/test-system-switches/.travis-ocaml.sh
 
 export OPAM_INIT=false
 bash -ex .travis-ocaml.sh
